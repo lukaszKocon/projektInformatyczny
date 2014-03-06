d3289eef060b5f79620470a1ac1bd7ddb701ecca
Lucas <kocon91@o2.pl>
2014-03-06 22:50:04 +0100

    v1.0

--- GIT/src/git/GIT.java
+++ GIT/src/git/GIT.java
@@ -14,6 +14,7 @@ public class GIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
 }
