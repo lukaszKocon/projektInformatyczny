12e64af25f3ad1cde76c6d41329176e5ed1014ef
Lucas <kocon91@o2.pl>
2014-03-06 23:15:22 +0100

    v1.7

--- GIT/src/git/GIT.java
+++ GIT/src/git/GIT.java
@@ -15,6 +15,7 @@ public class GIT {
      */
     public static void main(String[] args) {
         
-        System.out.print(" ");
+        System.out.print("   ");
+        System.out.print("   ");
     }
 }
