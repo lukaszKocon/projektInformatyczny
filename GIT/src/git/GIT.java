989573d83394658d9652bdc2eb52e3a57801d29f
Lucas <kocon91@o2.pl>
2014-03-06 23:23:09 +0100

    v1.8

--- GIT/src/git/GIT.java
+++ GIT/src/git/GIT.java
@@ -16,6 +16,6 @@ public class GIT {
     public static void main(String[] args) {
         
         System.out.print("   ");
-        System.out.print("   ");
+        System.out.print(" elo");
     }
 }
